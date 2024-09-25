cebe48d334a2f7bcd15d7ee511c3f452afe972e0
DUDHAT HEMIL PRAVINKUMAR <138382078+Hemil36@users.noreply.github.com>
2024-09-25 23:51:40 +0530

    Update App.jsx

--- FrontEnd/ImgProcessSaaS/src/App.jsx
+++ FrontEnd/ImgProcessSaaS/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import TempShow from "./pages/TempShow"
 import TempShow2 from './pages/TempShow2';
+import Login from './pages/Login.jsx';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login />} />
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/filters" element={<TempShow></TempShow>}></Route>
         <Route path="/filters2" element={<TempShow2></TempShow2>}></Route>
