168550d4cc0c2a54b2025e0455ad34fbcb25e2f3
Shivang Bhavesh Mewada <114918884+Shivang24012004@users.noreply.github.com>
2024-11-10 01:32:30 +0530

    undo jwt

--- BackEnd/routes/login.py
+++ BackEnd/routes/login.py
@@ -9,23 +9,23 @@ from dotenv import load_dotenv
 import time
 import jwt
 
-SECRET_KEY=os.getenv("JWT_SECRET_KEY")
-ALGO=os.getenv("JWT_ALGORITHM")
-ACCESS_TOKEN_EXPIRE_DAYS=4
+# SECRET_KEY=os.getenv("JWT_SECRET_KEY")
+# ALGO=os.getenv("JWT_ALGORITHM")
+# ACCESS_TOKEN_EXPIRE_DAYS=4
 
 load_dotenv()
 
 router=APIRouter()
 
-def create_access_token(data:dict,expire_delta=90000):
-    to_encode=data.copy()
-    expire=time.time()+expire_delta
-    to_encode.update({"exp":expire})
-    encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGO)
-    return encoded_jwt
+# def create_access_token(data:dict,expire_delta=90000):
+#     to_encode=data.copy()
+#     expire=time.time()+expire_delta
+#     to_encode.update({"exp":expire})
+#     encoded_jwt=jwt.encode(to_encode,SECRET_KEY,algorithm=ALGO)
+#     return encoded_jwt
 
 @router.post("/auth/signin")
-async def sign_in(user:UserCredentials,response:Response):
+async def sign_in(user:UserCredentials):
     try:
         existing_user=db["users"].find_one({"email":user.email})
         if not existing_user:
@@ -38,20 +38,14 @@ async def sign_in(user:UserCredentials,response:Response):
         user_data["_id"] = str(user_data["_id"])
         del user_data["password"]
         
-        access_token_expires = ACCESS_TOKEN_EXPIRE_DAYS * 24 * 60 * 60  # 4 days in seconds
-        access_token = create_access_token(
-            data={"sub": str(user_data["_id"])}, expire_delta=access_token_expires
-        )
-        # print(access_token)
-        response=JSONResponse(content={"success": "true", "message": "Login Successful", "user": user_data}, status_code=200)
-        response.set_cookie(
-            key='access_token',
-            value=f"Bearer {access_token}",
-            httponly=True,
-            secure=False,
-            samesite='none'
-        )
-        return response
+        # access_token_expires = ACCESS_TOKEN_EXPIRE_DAYS * 24 * 60 * 60  # 4 days in seconds
+        # access_token = create_access_token(
+        #     data={"sub": str(user_data["_id"])}, expire_delta=access_token_expires
+        # )
+        # # print(access_token)
+        
+        
+        return JSONResponse(content={"success": "true", "message": "Login Successful", "user": user_data}, status_code=200)
     except Exception as e:
         return JSONResponse(content={"error": "User could't be loggedin! {}".format(str(e))}, status_code=500)
     
